bd4a9d84b92a07bd6e520b6b7c1b00365b5b29e3
ShuABE1616 <125678937+ShuABE1616@users.noreply.github.com>
2024-02-07 20:23:47 +0900

    Create distance.ino

---
+++ examples/distance/distance.ino
@@ -0,0 +1,22 @@
+#include <takiroboF1.h>
+
+//HMC5883L(地磁気), MPU(ジャイロ), NONE(無し) から選択する
+//何も指定しない場合, コンパスは使用されない
+takiroboF1 robot;
+
+void setup()
+{
+  Serial.begin(9600);
+  robot.init();
+  //ここは電源を入れてから一度しか実行されません。
+}
+
+void loop()
+{
+  //プログラムはここをループし続けます。
+  if(robot.getBtn() == HIGH)//スタートスイッチが押された時
+  {
+    Serial.print("距離:");
+    Serial.println(robot.getUSS());
+  }
+}
